04cc2c7afee6c7a4efdc66a4dc1d606321c4bd4c
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-08-01 17:23:56 +0600

    search in table, realized using jQuery

--- converter/src/main/resources/static/js/converter.js
+++ converter/src/main/resources/static/js/converter.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
         "scrollCollapse": true,
         "ordering" : false,
         "language": {
-        "zeroRecords": "Вы еще не производили конвертаций!",
+        "search": "Поиск:",
+        "lengthMenu": "Показать _MENU_ записей.",
+        "zeroRecords": "Поиск не дал результатов!",
+        "infoEmpty": "Нет сохраненных конвертаций!",
+        "info": "Показана страница _PAGE_ из _PAGES_",
+        "infoFiltered": "(отфильтровано из _MAX_ записей)",
                 "oPaginate": {
                     "sNext":    "След.",
                     "sPrevious": "Пред."
